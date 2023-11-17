2ab253e2fbe13f5d18f25a58ece735adef61528f
Ruulul <vamjunior01@gmail.com>
2023-11-17 12:14:10 -0300

    default config for anyone who wants to compile locally

---
+++ src/default_config.zig
@@ -0,0 +1,20 @@
+pub const initial_creature_count = 100;
+pub const max_creature_count = 100;
+pub const initial_food_count = 50;
+pub const max_food_count = 200;
+
+pub const max_fitting_genomes = 20;
+
+pub const genome_length = 12;
+pub const intern_neurons_max = 5;
+
+pub const initial_energy = 255;
+pub const energy_loss_per_iteration = 1;
+pub const energy_loss_per_movement = 1;
+pub const energy_loss_per_replication = 10;
+pub const food_energy = 100;
+pub const chomps_to_be_selected = 2;
+
+/// currently in frames instead of iterations
+pub const spawn_food_interval = 25;
+pub const fps = 60;
